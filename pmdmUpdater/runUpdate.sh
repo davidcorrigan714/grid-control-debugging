c207eedad91766f402d154206012b5bc56744a81
David Corrigan <david.corrigan@ni.com>
2019-04-08 15:33:12 -0500

    Dockerfile WIP

--- pmdmUpdater/runUpdate.sh
+++ pmdmUpdater/runUpdate.sh
@@ -4,4 +4,4 @@
 #docker run -d --rm -e AZDO_USER_EMAIL=productadmin@ni.com -e AZDO_ADMIN_PWD pmdmupdater
 
 #DNS weirdness in Ubuntu 18.04 - https://github.com/docker/libnetwork/issues/2187
- docker run -d --rm  -e AZDO_USER_EMAIL=productadmin@ni.com -e AZDO_ADMIN_PWD=pmdmUpdater --dns `cat /run/systemd/resolve/resolv.conf | grep nameserver | head -n 1 | awk '{print $2}'` pmdmupdater
+docker run -d --rm -e AZDO_USER_EMAIL=productadmin@ni.com -e AZDO_ADMIN_PWD=pmdmUpdater --dns `cat /run/systemd/resolve/resolv.conf | grep nameserver | head -n 1 | awk '{print $2}'` pmdmupdater
