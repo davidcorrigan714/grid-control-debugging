03b66550d8920f57e76a17a072e4cd162c8d4e2a
David Corrigan <david.corrigan@ni.com>
2019-04-09 08:44:52 -0500

    Dockerfile update

--- pmdmUpdater/runUpdate.sh
+++ pmdmUpdater/runUpdate.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-#DNS weirdness line is for in Ubuntu 18.04 - https://github.com/docker/libnetwork/issues/2187
-
+#DNS weirdness in Ubuntu 18.04 - https://github.com/docker/libnetwork/issues/2187
 docker run -i --init --rm --cap-add=SYS_ADMIN \
-   -e AZDO_USER_EMAIL=email@ni.com -e AZDO_USER_PWD=password \
+   -e AZDO_USER_EMAIL=productadmin@ni.com -e AZDO_USER_PWD=mypassword \
    --dns `cat /run/systemd/resolve/resolv.conf | grep nameserver | head -n 1 | awk '{print $2}'` \
-   --name puppeteer-chrome pmdmupdater \
-   node -e "`cat update.js`"
-
+   --name pmdmupdater pmdmupdater
