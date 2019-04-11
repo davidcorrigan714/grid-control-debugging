cf2377806b58c20c9ca584f71b415a62446d61c1
David Corrigan <david.corrigan@ni.com>
2019-04-11 11:37:40 -0500

    add daemon argument to docker script

--- pmdmupdater/runPMDMUpdate.sh
+++ pmdmupdater/runPMDMUpdate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #DNS weirdness in Ubuntu 18.04 - https://github.com/docker/libnetwork/issues/2187
-docker run -i --init --rm --cap-add=SYS_ADMIN \
+docker run -d -i --init --rm --cap-add=SYS_ADMIN \
    -e AZDO_USER_EMAIL=productadmin@ni.com -e AZDO_USER_PWD=mypassword \
    --dns `cat /run/systemd/resolve/resolv.conf | grep nameserver | head -n 1 | awk '{print $2}'` \
    --name pmdmupdater pmdmupdater
