326a8314e262f804d82c9ed355ad3ea3e09d0ea7
David Corrigan <david.corrigan@ni.com>
2019-04-19 20:03:38 +0000

    pmdmConnector in daemon

--- pmdmConnector/runPMDMConnector.sh
+++ pmdmConnector/runPMDMConnector.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #DNS weirdness in Ubuntu 18.04 - https://github.com/docker/libnetwork/issues/2187
-docker run -i --init --rm -p 443:443 \
+docker run -i --init --rm -p 443:443 -d \
    -e SERVERNAME=azdo-dev.natinst.com \
    --dns `cat /run/systemd/resolve/resolv.conf | grep nameserver | head -n 1 | awk '{print $2}'` \
    --name pmdmconnector pmdmconnector
