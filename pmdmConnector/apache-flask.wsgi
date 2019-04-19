9d06938f20a34121c07f2522bdb3cb735b559d65
David Corrigan <david.corrigan@ni.com>
2019-04-19 14:57:29 -0500

    pmdmConnector Docker

--- pmdmConnector/apache-flask.wsgi
+++ pmdmConnector/apache-flask.wsgi
@@ -1,5 +1,5 @@
-#! /usr/bin/python
+#!/usr/bin/python3
 
-from app import app as application
 import sys
 sys.path.append("/var/www/apache-flask")
+from pmdmConnector import app as application
--- pmdmConnector/runPMDMConnector.sh
+++ pmdmConnector/runPMDMConnector.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #DNS weirdness in Ubuntu 18.04 - https://github.com/docker/libnetwork/issues/2187
-docker run -d -i --init --rm
+docker run -i --init --rm -p 443:443 \
+   -e SERVERNAME=azdo-dev.natinst.com \
    --dns `cat /run/systemd/resolve/resolv.conf | grep nameserver | head -n 1 | awk '{print $2}'` \
-   --name pmdmConnector pmdmConnector
+   --name pmdmconnector pmdmconnector
